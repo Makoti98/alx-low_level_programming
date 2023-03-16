32cbca12b6292620fddbe56b3cb7da56ab6d4093
makoti98 <athumanimakoti@gmail.com>
2023-03-16 13:56:08 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- this print the main string in the function,
+ * it is also the header o c languge.
+ *
+ * Description: this program is suppossed to print the following line
+ * with proper grammar, but the outcome is a piece of art,.
+ * return: it normally returns nill results (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
