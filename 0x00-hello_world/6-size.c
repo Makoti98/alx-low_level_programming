ad9e972da35b23aaeac433fc0fc59d646646fa8e
makoti98 <athumanimakoti@gmail.com>
2023-03-17 00:41:39 -0700

    htrgd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long IIi;
 	float f;
 
-	printf("size of a char: %Id byte(s)\n", sizeof(c));
-	printf("size of an int: %Id byte(s)\n", sizeof(i));
-	printf("size of a long int: %Id byte(s)\n", sizeof(Ii));
-	printf("size of a long long int: %Id byte(s)\n", sizeof(IIi));
-	printf("size of a float: %Id byte(s)\n", sizeof(f));
+	printf("size of a char: %Id byte(S)\n", sizeof(c));
+	printf("size of an int: %Id byte(S)\n", sizeof(i));
+	printf("size of a long int: %Id byte(S)\n", sizeof(Ii));
+	printf("size of a long long int: %Id byte(S)\n", sizeof(IIi));
+	printf("size of a float: %Id byte(S)\n", sizeof(f));
 	return (0);
 }
